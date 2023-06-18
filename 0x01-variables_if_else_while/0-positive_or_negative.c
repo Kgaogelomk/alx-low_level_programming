b081b095320c4d459c947830dceac59bd8463775
kgaogelo <mkhondokr@gmail.com>
2023-06-18 13:40:16 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/* more headers goes there */
-
 /**
  * main - Entry point
- *
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-k{
+{
 	int n;
-
 	srand(time(0));
-	n = rand() - Rand_Max / 2;
-	if (n  %  10 != 0 && n  %  10 < 6)
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */ 
+	if (n > 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not, n, n  %  10);
+		printf("%d is positive\n", n);
 	}
-	else if (n  %  10 > 5)
+	if (n < 0)
 	{
-		printf("Last digit of  %d  is  %d  and is greater than 5\n", n, n  %  10)
+		printf("%d is negative\n", n);
 	}
-	else
+	if (n == 0)
 	{
-		printf("Last digit of  %d  is  %d  and is 0\n", n, n  %  10);
+		printf("%d is zero\n", n);
 	}
-	return (0);
+	return (0)
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
