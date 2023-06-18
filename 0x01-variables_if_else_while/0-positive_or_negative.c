a36a798b2fc901fecace6ee4e037a5e3d8e1dfb6
kgaogelo <mkhondokr@gmail.com>
2023-06-18 10:42:41 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
 /**
- * main - determines if a number is positive, negative or zero.
+ * main - Entry point
  *
- * return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
+k{
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	n = rand() - Rand_Max / 2;
+	if (n  %  10 != 0 && n  %  10 < 6)
 	{
-		printf("%d is positive\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not, n, n  %  10);
 	}
-	else if (n == 0)
+	else if (n  %  10 > 5)
 	{
-		printf("%d is zero\n", n);
+		printf("Last digit of  %d  is  %d  and is greater than 5\n", n, n  %  10)
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("Last digit of  %d  is  %d  and is 0\n", n, n  %  10);
 	}
 	return (0);
+}
